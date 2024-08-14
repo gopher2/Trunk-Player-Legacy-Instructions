5ee24d91e3da396647d5a14d43eabb0691ca063b
gopher2 <davidkierz@gmail.com>
2024-08-13 22:22:19 -0400

    Update postgres_setup.sql

--- postgres_setup.sql
+++ postgres_setup.sql
@@ -1,6 +1,13 @@
 CREATE USER trunk_player_user WITH PASSWORD '__DB_PASS__';
-CREATE DATABASE trunk_player;
+CREATE DATABASE trunk_player OWNER trunk_player_user;
 GRANT ALL PRIVILEGES ON DATABASE trunk_player TO trunk_player_user;
 ALTER ROLE trunk_player_user SET client_encoding TO 'utf8';
 ALTER ROLE trunk_player_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE trunk_player_user SET timezone TO 'UTC';
+\c trunk_player
+GRANT USAGE ON SCHEMA public TO trunk_player_user;
+GRANT CREATE ON SCHEMA public TO trunk_player_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO trunk_player_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO trunk_player_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO trunk_player_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO trunk_player_user;
